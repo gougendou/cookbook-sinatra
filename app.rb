a323c35a074c8fb91897a7798470ec54a18ea496
SamTreon <stat123@hotmail.co.uk>
2023-02-07 13:55:37 +0000

    not sure about this

--- app.rb
+++ app.rb
@@ -3,11 +3,23 @@ require "sinatra/reloader" if development?
 require "pry-byebug"
 require "better_errors"
 
+set :bind, "0.0.0.0"
+
 configure :development do
   use BetterErrors::Middleware
   BetterErrors.application_root = File.expand_path(__dir__)
 end
 
 get "/" do
-  "Let's goooooo!"
+  @usernames = ["ssaunier", "Papillard"]
+  erb :index
+end
+
+get "/about" do
+  erb :about
+end
+
+get "/team/:username" do
+  puts params[:username]
+  "The username is #{params[:username]}"
 end
